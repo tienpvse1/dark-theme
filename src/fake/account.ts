bc87dd07da54e9001fdf188da1beac36c8f600e2
tienpvse1 <tienpvse@fpt.edu.vn>
2022-01-01 09:18:29 +0700

    resolved(code): fix size generate bug

--- src/fake/account.ts
+++ src/fake/account.ts
@@ -3,7 +3,7 @@ import { Account } from "../interface/account/entity/account";
 // by default, 10 random account will be generated
 export const getRandomAccounts = (size = 10): Account[] => {
   const accounts: Account[] = [];
-  for (let index = 0; index < 10; index++) {
+  for (let index = 0; index < size; index++) {
     accounts.push({
       id: index.toString(),
       address: address.streetAddress(),
